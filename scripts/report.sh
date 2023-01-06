5a2241ed231eee12ddd3a1b92cd27202dd0d6797
Or Zeira <orz@neftun.com>
2023-01-06 18:01:19 +0200

    installed dependencies as part of the report script

--- scripts/report.sh
+++ scripts/report.sh
@@ -5,6 +5,7 @@ docker pull cloudcustodian/c7n
 rm -r output/
 mkdir output
 
+# Search for orphan resources
 docker run -it \
   -v $(pwd)/output:/home/custodian/output \
   -v $(pwd)/policy.yml:/home/custodian/policy.yml \
@@ -13,4 +14,5 @@ docker run -it \
   --env-file <(env | grep "^AWS") \
   cloudcustodian/c7n run --dryrun --cache-period 0 -s /home/custodian/output /home/custodian/policy.yml
 
-npm run build && ts-node src/index.ts
+# Run cost report
+npm install && npm run build && ts-node src/index.ts
